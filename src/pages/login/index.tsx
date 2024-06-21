75ca545c11250ea3836a95b76e0b7958e532cd77
Alex Nascimento <alexjose68@hotmail.com>
2024-06-21 14:24:55 -0300

    feat: instalei o react-hook-form, o hookform/resolvers e o zod para criar formulários perfomáticos e com validações sem necessidade de usar useStates para cada campo do formulário, o que deixa a aplicação mais leve e perfomática, também criei um componente de Input passando interface com prorpiedades que servirão também para validação dos dodos do formulário como também criei a página de Login com um schema de validação usando o zod com o formulário controlado pelo react-hook-form

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,73 @@
+import Logo from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
+import { Container } from '../../components/container';
+import { Input } from '../../components/input';
+
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const schema = z.object({
+  email: z.string().min(1, { message: "O campo email é obrigatório" }).email("Insira um email válido"),
+  password: z.string().min(1, { message: "O campo senha é obrigatório" })
+});
+
+type FormData = z.infer<typeof schema>
+
 export function Login() {
+
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onChange"
+  });
+
+  function onSubmit(data: FormData) {
+    console.log(data);
+  }
+
   return (
-    <div>
-      <h1>Página de Login</h1>
-    </div>
+    <Container>
+      <div className='w-full min-h-screen flex justify-center items-center flex-col gap-4'>
+        <Link to="/" className="mb-6 max-w-sm w-full">
+          <img
+            src={Logo}
+            alt="Logo do Site"
+            className='w-full'
+          />
+        </Link>
+
+        <form
+          className='bg-white w-full max-w-xl rounded-lg p-4'
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className='mb-3'>
+            <Input
+              type="email"
+              placeholder="Digite seu email..."
+              name="email"
+              error={errors.email?.message}
+              register={register}
+            />
+          </div>
+
+          <div className='mb-3'>
+            <Input
+              type="password"
+              placeholder="Digite sua senha..."
+              name="password"
+              error={errors.password?.message}
+              register={register}
+            />
+          </div>
+
+          <button
+            type='submit'
+            className='bg-zinc-900 w-full rounded-md text-white h-10 font-medium'
+          >
+            Acessar
+          </button>
+        </form>
+      </div>
+    </Container>
   )
 }

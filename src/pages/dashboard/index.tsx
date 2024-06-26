3432647a14c338ac6692675858df14e78f7ddaff
Alex Nascimento <alexjose68@hotmail.com>
2024-06-26 11:20:02 -0300

    feat: adicionando funcionalidade de deletar as imagens do carro deletado no storage do firebase na página de dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -7,7 +7,8 @@ import { FiTrash2} from 'react-icons/fi';
 import { AuthContext } from '../../contexts/AuthContext';
 
 import { collection, getDocs, where, query, doc, deleteDoc } from 'firebase/firestore';
-import { db } from '../../services/firebaseConnection';
+import { db, storage } from '../../services/firebaseConnection';
+import { ref, deleteObject } from 'firebase/storage';
 
 interface CarProps {
   id: string;
@@ -65,10 +66,26 @@ export function Dashboard() {
     loadCars();
   }, [user]);
 
-  async function handleDeleteCar(id: string) {
-    const docRef = doc(db, "cars", id);
+  async function handleDeleteCar(car: CarProps) {
+    const itemCar = car;
+    
+    const docRef = doc(db, "cars", itemCar.id);
     await deleteDoc(docRef);
-    setCars(cars.filter(car => car.id !== id));
+
+    itemCar.images.map( async (image) => {
+      const imagePath = `images/${image.uid}/${image.name}`;
+      const imageRef = ref(storage, imagePath);
+
+      try {
+        await deleteObject(imageRef);
+        setCars(cars.filter(car => car.id !== itemCar.id));
+
+      } catch(err) {
+        console.log("ERRO AO EXCLUIR ESSA IMAGEM", err);
+      }
+
+    });
+
   }
 
   return (
@@ -79,7 +96,7 @@ export function Dashboard() {
         {cars.map( car => (
           <section key={car.id} className="w-full bg-white rounded-lg relative">
             <button
-              onClick={ () => handleDeleteCar(car.id) }
+              onClick={ () => handleDeleteCar(car) }
               className="absolute bg-white w-14 h-14 rounded-full flex items-center justify-center right-2 top-2 drop-shadow"
             >
               <FiTrash2 size={26} color="#000" />

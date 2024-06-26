6a87a4d7628f530ff8d4f5b80d21c7981bf73f85
Alex Nascimento <alexjose68@hotmail.com>
2024-06-25 21:36:54 -0300

    feat: criamos a lógica da lista de carros adicionados pelo usuário que está logado para passar na página de dashboard/painel, passamos um useState cars para salvar a lista, e pegando a informação do usuário logado pelo value user do context AuthContext criamos a lógica necessária para acessar apenas os carros cadastrados por esse determiando usuário, também criamos um botão para excluir o carroe para isso pegamos o o id do carro para deletá-lo quando o usuário clicar neste botão

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,11 +1,116 @@
+import { useEffect, useState, useContext } from "react";
 import { Container } from "../../components/container";
 import { DashboardHeader } from "../../components/painelheader";
 
+import { FiTrash2} from 'react-icons/fi';
+
+import { AuthContext } from '../../contexts/AuthContext';
+
+import { collection, getDocs, where, query, doc, deleteDoc } from 'firebase/firestore';
+import { db } from '../../services/firebaseConnection';
+
+interface CarProps {
+  id: string;
+  name: string;
+  year: string;
+  price: string | number;
+  city: string;
+  km: string,
+  images: ImageProps[];
+  uid: string;
+}
+
+interface ImageProps {
+  name: string;
+  uid: string;
+  url: string;
+}
+
 export function Dashboard() {
+  const [cars, setCars] = useState<CarProps[]>([]);
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+
+    function loadCars() {
+      if(!user?.uid) {
+        return;
+      }
+
+      const carsRef = collection(db, "cars");
+      const queryRef = query(carsRef, where("uid", "==", user.uid));
+
+      getDocs(queryRef)
+      .then((snapshot) => {
+        let listcars = [] as CarProps[];
+
+        snapshot.forEach( doc => {
+          listcars.push({
+            id: doc.id,
+            name: doc.data().name,
+            year: doc.data().year,
+            km: doc.data().km,
+            city: doc.data().city,
+            price: doc.data().price,
+            images: doc.data().images,
+            uid: doc.data().uid
+          })
+        })
+
+        setCars(listcars);
+        console.log(listcars);
+      })
+    }
+
+    loadCars();
+  }, [user]);
+
+  async function handleDeleteCar(id: string) {
+    const docRef = doc(db, "cars", id);
+    await deleteDoc(docRef);
+    setCars(cars.filter(car => car.id !== id));
+  }
+
   return (
     <Container>
       <DashboardHeader />
-      <h1>Página de Dashboard</h1>
+      <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+        
+        {cars.map( car => (
+          <section key={car.id} className="w-full bg-white rounded-lg relative">
+            <button
+              onClick={ () => handleDeleteCar(car.id) }
+              className="absolute bg-white w-14 h-14 rounded-full flex items-center justify-center right-2 top-2 drop-shadow"
+            >
+              <FiTrash2 size={26} color="#000" />
+            </button>
+
+            <img
+              className="w-full rounded-lg mb-2 max-h70"
+              src={car.images[0].url}
+              alt=""
+            />
+            <p className="font-bold mt-1 px-2 mb-2">{car.name}</p>
+
+            <div className="flex flex-col px-2">
+              <span className="text-zinc-700">
+                Ano {car.year} | {car.km} km
+              </span>
+              <strong className="text-black font-bold mt-4">
+                R$ {car.price}
+              </strong>
+            </div>
+
+            <div className="w-full h-px bg-slate-200 my-2"></div>
+            <div className="px-2 pb-2">
+              <span className="text-black">
+                {car.city}
+              </span>
+            </div>
+
+          </section>
+        ))}
+      </main>
     </Container>
   )
 }

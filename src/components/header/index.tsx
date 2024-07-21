d659ab6d09840121c997f2cb709b1777427ba402
Alex Nascimento <alexjose68@hotmail.com>
2024-07-21 10:45:20 -0300

    feat: passei para o Header o nome no usuário que está logado e a palavra Entrar caso não haja usuário logado para ficar mais fácil identificar s situação de logado e não logado. Também mudei o caminho do favicon em index.html

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -6,7 +6,7 @@ import { FiUser, FiLogIn } from 'react-icons/fi'
 
 export function Header() {
 
-  const {signed, loadingAuth} = useContext(AuthContext);
+  const {signed, loadingAuth, user} = useContext(AuthContext);
 
   return (
     <div className="w-full flex items-center justify-center h-16 bg-white drop-shadow mb-4">
@@ -19,19 +19,21 @@ export function Header() {
         </Link>
 
         {!loadingAuth && signed && (
-          <Link to="/dashboard">
-          <div className="border-2 rounded-full p-1 border-gray-900">
-            <FiUser size={22} color="#000" />
-          </div>
-        </Link>
+          <Link to="/dashboard" className='flex items-center gap-2'>
+            <span>{user?.name}</span>
+            <div className="border-2 rounded-full p-1 border-gray-900">
+              <FiUser size={22} color="#000" />
+            </div>
+          </Link>
         )}
 
         {!loadingAuth && !signed && (
-          <Link to="/dashboard">
-          <div className="border-2 rounded-full p-1 border-gray-900">
-            <FiLogIn size={22} color="#000" />
-          </div>
-        </Link>
+          <Link to="/dashboard" className='flex items-center gap-2'>
+            <span>Entrar</span>
+            <div className="border-2 rounded-full p-1 border-gray-900">
+              <FiLogIn size={22} color="#000" />
+            </div>
+          </Link>
         )}
 
       </header>
